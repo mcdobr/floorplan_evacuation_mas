d4be775e24de7f8e2b149910b7e38726ed8b5670
mcdobr <mircea.dobreanu@gmail.com>
2021-01-16 13:02:56 +0200

    Create basic message starting point

---
+++ floorplan_evacuation_mas/FloorPlanMessage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace floorplan_evacuation_mas
+{
+    public class FloorPlanMessage
+    {
+        private string type { get; set; }
+        private Tuple<int, int> position { get; set; }
+        private List<Tuple<int, int>> exitsInFieldOfViewPositions { get; set; }
+        private List<Tuple<int, int>> agentsInFieldOfViewPositions { get; set; }
+    }
+}
